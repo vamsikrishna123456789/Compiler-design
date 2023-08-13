ef6a39cb8fed84bee636afae44cbe0b6d230ca52
vamsikrishna123456789 <112241623+vamsikrishna123456789@users.noreply.github.com>
2023-08-13 20:57:23 +0530

    Add files via upload

---
+++ 5.findwhitespaceandnewline.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+
+    int words=0,newline=0,characters=0; 
+
+    scanf("%[^~]",&str);  
+
+    for(int i=0;str[i]!='\0';i++)
+     { 
+         if(str[i] == ' ')
+         { 
+              words++;
+         }
+         else if(str[i] == '\n')
+         {
+             newline++;
+              words++;
+         }
+         else if(str[i] != ' ' && str[i] != '\n'){
+         characters++;
+         }
+     }
+    if(characters > 0)
+    {
+        words++;
+        newline++;
+    }
+     printf("Total number of words : %d\n",words);
+     printf("Total number of lines : %d\n",newline);
+     printf("Total number of characters : %d\n",characters);
+    return 0;
+}
+output:
+void main()
+{
+int a;
+int b;
+a = b + c;
+c = d * e;
+}
+Total number of words : 18
+Total number of lines : 7
